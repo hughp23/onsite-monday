e050790d5baa3f1f44a75de55ff7353e07bdfeee
“Hugh“ <hughpaul14@gmail.com>
2026-09-11 08:34:14 +0100

    chore: remove unused EF Core using in PaymentFlowIntegrationTests

--- backend/tests/OnsiteMonday.Api.Tests/Integration/PaymentFlowIntegrationTests.cs
+++ backend/tests/OnsiteMonday.Api.Tests/Integration/PaymentFlowIntegrationTests.cs
@@ -1,7 +1,6 @@
 using System.Net;
 using System.Net.Http.Json;
 using FluentAssertions;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using OnsiteMonday.Api.Data;
 using OnsiteMonday.Api.Domain;
